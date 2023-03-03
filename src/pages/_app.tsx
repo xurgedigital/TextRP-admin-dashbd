c47f08cd42118b208f414930840f3dc0069e0b65
himanshutfw <125906462+himanshutfw@users.noreply.github.com>
2023-03-03 19:40:46 +0530

    login - in progress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { XummProvider } from "xumm-react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const config = {
-    url: "",
-    getToken: () => (console.log("")),
-    setToken: () => (console.log("")),
-    removeToken: () => (console.log("")),
-  };
   return (
-    <XummProvider config={config}>
       <Component {...pageProps} />
-    </XummProvider>
   );
 }
