6943a3eb1ccbca59a13fe2e01ec4ba87524072cb
himanshutfw <125906462+himanshutfw@users.noreply.github.com>
2023-03-13 20:03:35 +0530

    added swr

--- src/pages/setting/index.tsx
+++ src/pages/setting/index.tsx
@@ -1,19 +1,7 @@
 import Settings from '@/components/Settings'
 import DummyChatSection from '@/components/DummyChatSection'
-import { useRouter } from 'next/router'
-import { useContext, useEffect } from 'react'
-import { Context } from '../_app'
 
 export default function SettingPage() {
-  const router = useRouter()
-  const { state, dispatch }: any = useContext(Context)
-
-  useEffect(() => {
-    if (!(state.user.isLoggedIn || localStorage.getItem('isLoggedIn') == 'true')) {
-      router.push('/login')
-    }
-  })
-
   return (
     <div className="flex max-h-screen">
       <Settings />
